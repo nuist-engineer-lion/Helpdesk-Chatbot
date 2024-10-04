5854d227e0bc20ebbdd1d7fbe704732236b30aff
jerry-harm <109275116+jerry-harm@users.noreply.github.com>
2024-10-04 09:27:15 +0800

    use lark api instead of nonebot adapter

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
-from nonebot.adapters.feishu import Adapter as 飞书Adapter
-
-
-
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
-
-driver.register_adapter(飞书Adapter)
 
 nonebot.load_plugin("noticebot.plugins.bridge")  
 
--- noticebot/plugins/bridge.py
+++ noticebot/plugins/bridge.py
@@ -1,23 +1,37 @@
 import nonebot
 from nonebot.rule import to_me
 from nonebot.plugin import on_message
-from nonebot.adapters.feishu import Bot as FeishuBot
-from nonebot.adapters.onebot.v11 import Bot as OneBot
 from nonebot import get_bot
 from nonebot.adapters import Event
+import lark_oapi as lark
+import json
 import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 feishu_group: str = os.getenv('FEISHU_GROUP')
+app_id:str = os.getenv('APP_ID')
+app_secret:str = os.getenv('APP_SECRET')
 
-feishu_bot_id = nonebot.get_driver().config.feishu_bots[0]['app_id']
-test = on_message(rule=to_me(),priority=10)
+lark_client = lark.Client.builder().app_id(app_id).app_secret(app_secret).build()
+
+test = on_message(rule=to_me(), priority=10)
 
 @test.handle()
-async def handle_function(event:Event):
-    feishu:FeishuBot = get_bot(feishu_bot_id)
-    feishu.send_msg(receive_id_type="chat_id",
-                    receive_id=feishu_group,
-                    content=f"{event.get_session_id} says:\n{event.get_message}",
-                    msg_type='text'
-                    )
+async def handle_function(event: Event):
+    msg = f'{event.get_session_id()} says:{event.get_plaintext()}'
+    msg_json= json.dumps({"text":msg,"type":"template"},ensure_ascii=False)
+    request: lark.api.im.v1.CreateMessageRequest = lark.api.im.v1.CreateMessageRequest.builder() \
+        .receive_id_type("chat_id") \
+        .request_body(lark.api.im.v1.CreateMessageRequestBody.builder()
+            .receive_id(feishu_group)
+            .msg_type("text")
+            .content(msg_json)
+            .build()) \
+        .build()
+
+    # 发起请求
+    response = lark_client.im.v1.message.create(request)
+    if not response.success():
+        lark.logger.error(
+        f"client.request failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
