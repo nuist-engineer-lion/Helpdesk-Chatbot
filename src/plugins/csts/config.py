78be344bc554b04dbd3fd88ef74df90a3f28dfee
faithleysath <faithleysath@gmail.com>
2024-10-07 12:49:35 +0800

    refactor: :recycle: 清理仓库，准备重构

--- src/plugins/csts/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Config(BaseModel):
-    """Plugin Config Here"""
-    notify_group: str|int
-    engineers: List[str]
-    waiting_time: int = 30
