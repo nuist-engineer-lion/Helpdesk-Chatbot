42e62c114e3e820ac57526ce71853671cbc7baa2
faithleysath <faithleysath@gmail.com>
2024-10-08 00:50:29 +0800

    refactor: :recycle: 重构字段

--- src/plugins/csts/model.py
+++ src/plugins/csts/model.py
@@ -10,13 +10,13 @@ class Ticket(Model):
     customer_id = Column(String)
     engineer_id = Column(String, nullable=True)
 
-    status = Column(String,default="creating")
+    status = Column(String,default="creating")# creating, processing, alarming, closed
     
     begin_at = Column(DateTime)
     end_at = Column(DateTime, nullable=True)
     
     creating_expired_at = Column(DateTime)
-    processing_expired_at = Column(DateTime)
+    alarming_expired_at = Column(DateTime)
     
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, onupdate=datetime.now)
