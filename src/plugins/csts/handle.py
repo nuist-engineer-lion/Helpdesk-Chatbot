7147c699a05f2e0b64e66c7f7aded86a126f1556
jerry-harm <109275116+jerry-harm@users.noreply.github.com>
2024-11-02 16:16:15 +0800

    修改加好友优先级

--- src/plugins/csts/handle.py
+++ src/plugins/csts/handle.py
@@ -507,7 +507,7 @@ async def who_asked(bot: Bot, event: MessageEvent):
 # 处理好友添加请求
 async def _friend_request(bot: Bot, event: Event, state: T_State) -> bool:
     return isinstance(event, FriendRequestEvent)
-friend_request = on_notice(_friend_request, priority=12, block=True)
+friend_request = on_notice(_friend_request, priority=1, block=True)
 
 
 @friend_request.handle()
