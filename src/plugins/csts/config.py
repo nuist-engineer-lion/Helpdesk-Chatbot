abe8499c9b27a9e725d4495244e58cd9f8df7355
Xuededmiaoi <xuededmiao@outlook.com>
2024-11-13 20:48:54 +0800

    Update config.py
    添加了
    要求客户详细描述的提示信息
    告知客户稍等的提示信息
    客户第一次回复消息字数对比

--- src/plugins/csts/config.py
+++ src/plugins/csts/config.py
@@ -20,6 +20,9 @@ class Config(BaseModel):
     alarm_ticket_notify: str = "用户在催单啦！请尽快处理！"  # 工单报警通知消息
 
     first_reply: str = "您好，欢迎联系咨询，请描述您的问题。"  # 第一次回复消息
+    further_short: str = "可以详细一点吗"     # 要求客户详细描述的提示信息
+    further_long: str = "好的，稍等。"     # 告知客户稍等的提示信息
+    len_of_reply = 5  # 客户第一次回复消息字数对比
     first_reply_delay: int = 5  # 秒，第一次回复延迟时间
     # second_reply: str = "我们已经收到您的问题，正在为您联系工程师，请稍等片刻。"# 第二次回复消息
     # third_reply: str = "正在为您加急联系中，请稍等片刻。"# 第三次回复消息
