85315f4960dd264cb53f7df4e68f1f3ce53734a1
jerry-harm <109275116+jerry-harm@users.noreply.github.com>
2025-05-22 09:42:56 +0800

    install feishu

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-
+from nonebot.adapters.feishu import Adapter as FeishuAdapter
 
 
 nonebot.init(chatrecorder_record_send_msg="False")
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
-
+driver.register_adapter(FeishuAdapter)
 
 nonebot.load_from_toml("pyproject.toml")
 
